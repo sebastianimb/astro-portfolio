6b499e1aea4126ce194728f9a9daf19dde6c8473
sebastian molina <cirp8918@gmail.com>
2026-01-21 12:32:02 -0300

    fix: update recipient email address in send-email function

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -40,7 +40,7 @@ exports.handler = async (event) => {
 
     await resend.emails.send({
       from: `${name} <${email}>`,
-      to: ["fabianmorag0405@gmail.com"],
+      to: ["cirp8918@gmail.com"],
       subject: `Nuevo contacto: ${name}`,
       html: `
         <h2>Nuevo mensaje de contacto</h2>
